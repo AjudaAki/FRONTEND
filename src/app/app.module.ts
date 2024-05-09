cd2841881515c9e780bb0bb3ad825b2bb1150076
Daniel Vieira <126349675+DNLVRDCNH@users.noreply.github.com>
2024-05-09 08:06:02 -0300

    alteração_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { CadastroUserComponent } from './components/cadastro-user/cadastro-user.component';
 import { LoginCadastroComponent } from './components/login-cadastro/login-cadastro.component';
 import { ModalProfComponent } from './components/modal-prof/modal-prof.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ModalProfComponent } from './components/modal-prof/modal-prof.component
     CadastroComponent,
     CadastroUserComponent,
     LoginCadastroComponent,
-    ModalProfComponent
+    ModalProfComponent,
+    HeaderComponent,
+    InicioComponent
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule, HttpClientModule],
   providers: [],
