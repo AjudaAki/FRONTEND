361ce19a68e6bde00ec610cb80fcf104858c1d20
Daniel Vieira <126349675+DNLVRDCNH@users.noreply.github.com>
2024-05-21 19:27:37 -0300

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  currentSlide = 0;
+  totalSlides = 3;  // Total number of slides
 
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.totalSlides) % this.totalSlides;
+  }
 }
