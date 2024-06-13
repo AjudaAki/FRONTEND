44ea26ef178703d7d3ed15984aee521700273104
Felslender <felipecavalcantedev1112005@gmail.com>
2024-06-13 19:06:44 -0300

    erro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+// Importações dos componentes
 import { HomeComponent } from './components/home/home.component';
 import { PerfilProfComponent } from './components/perfil-prof/perfil-prof.component';
 import { CadastroUserComponent } from './components/cadastro-user/cadastro-user.component';
 import { LoginCadastroComponent } from './components/login-cadastro/login-cadastro.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TelaPerfilComponent } from './components/tela-perfil/tela-perfil.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TelaProfessoresComponent } from './components/tela-professores/tela-professores.component';
 import { TelaFavoritosComponent } from './components/tela-favoritos/tela-favoritos.component';
 import { CardProfComponent } from './components/card-prof/card-prof.component';
@@ -27,10 +27,16 @@ import { CardProfComponent } from './components/card-prof/card-prof.component';
     TelaPerfilComponent,
     TelaProfessoresComponent,
     TelaFavoritosComponent,
-    CardProfComponent
+    CardProfComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
